319ba176e5f2566dee1e9c04a388b9aab7b60a87
Ziya <ziyakerimli04@gmail.com>
2023-06-05 10:20:27 +0300

    create task controller

---
+++ src/controllers/task.controller.ts
@@ -0,0 +1,87 @@
+import { ITask, ITaskP } from '../types/ITask';
+import { TaskService } from '../services/task.service';
+
+const ERROR_NAME = 'please specify name to create task';
+
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  async create(args: any) {
+    try {
+      if (!args['-n']) throw new Error(ERROR_NAME);
+      const name = args['-n'];
+      const done = '';
+      let [description, deadline] = ['', ''];
+      if (args['-i']) description = args['-i'];
+      if (args['-d']) deadline = args['-d'];
+      const task: ITask = { name, deadline, description, done };
+      const res = await this.taskService.createTask(task);
+      console.log(res);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  async showAll() {
+    try {
+      const res = await this.taskService.getAllTasks();
+      console.log(res);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  async showUndoneList() {
+    try {
+      const res = await this.taskService.getUndone();
+      console.log(res);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  async markDone(args: any) {
+    try {
+      if (!args['-n']) throw new Error(ERROR_NAME);
+      const name = args['-n'];
+      const res = await this.taskService.markDone(name);
+      console.log(res);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  async editTask(args: any) {
+    try {
+      if (!args['-n']) throw new Error(ERROR_NAME);
+      const name = args['-n'];
+      const edit: ITaskP = {};
+      if (args['-i']) edit.description = args['-i'];
+      if (args['-d']) edit.deadline = args['-d'];
+      const res = await this.taskService.editTask(name, edit);
+      console.log(res);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  async showOmitedList() {
+    try {
+      const res = await this.taskService.getOmited();
+      console.log(res);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  async removeTask(args: any) {
+    try {
+      if (!args['-n']) throw new Error(ERROR_NAME);
+      const name = args['-n'];
+      const res = await this.taskService.removeTask(name);
+      console.log(res);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+}
